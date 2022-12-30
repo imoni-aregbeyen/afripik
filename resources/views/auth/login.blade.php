55de1f3c5f1fdc24553c79833264976f63b7c686
Imonitie Aregbeyen <aregbeyenimonitie@gmail.com>
2022-12-30 09:00:20 +0100

    enabled login with email and password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,12 +10,12 @@
   <div class="row g-3">
     <div class="col-md-12">
         <label for="email" class="form-label">E-MAIL OR USERNAME</label>
-        <input type="email" class="form-control" id="email" required>
+        <input type="text" name="email" class="form-control" id="email" value="{{ old('email') }}" required autofocus>
     </div>
     <div class="col-md-12">
         <label for="password" class="form-label">PASSWORD</label>
         <div class="input-group">
-            <input type="password" class="form-control" id="password" required>
+            <input type="password" name="password" class="form-control" id="password" required autocomplete="current-password">
             <span class="btn btn-outline-secondary rounded-end" onclick="hidePassword()" id="btnHide">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
                     <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z"/>
